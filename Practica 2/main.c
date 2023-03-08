d94e116525590e85727550c88d84c464c0bc2193
GuillermoRdgz2 <g.rodriguezr@alumnos.upm.es>
2023-03-08 18:57:34 +0100

    Ejercicio 2 finalizado. Librería del RTC creada.

--- Practica 2/main.c
+++ Practica 2/main.c
@@ -40,6 +40,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "lcd.h"
+#include "rtc.h"
 #include <stdio.h>
 
 #ifdef RTE_CMSIS_RTOS2_RTX5
@@ -75,25 +76,11 @@ uint32_t HAL_GetTick (void) {
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
-#define RTC_ASYNCH_PREDIV  0x7F   /* LSE as RTC clock */
-#define RTC_SYNCH_PREDIV   0x00FF /* LSE as RTC clock */
-
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-/* RTC handler declaration */
-RTC_HandleTypeDef RtcHandle;
-
-/* Buffers used for displaying Time and Date */
-uint8_t aShowTime[50] = {0};
-uint8_t aShowDate[50] = {0};
-
 /* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 static void Error_Handler(void);
-
-/* RTC function prototypes ---------------------------------------------------*/
-void RTC_CalendarConfig(void);
-void RTC_CalendarShow(uint8_t *showtime, uint8_t *showdate);
 
 /* Private functions ---------------------------------------------------------*/
 /**
@@ -121,54 +108,7 @@ int main(void)
 
   /* Add your application code here
      */
-  /*##-1- Configure the RTC peripheral #######################################*/
-  /* Configure RTC prescaler and RTC data registers */
-  /* RTC configured as follows:
-      - Hour Format    = Format 24
-      - Asynch Prediv  = Value according to source clock
-      - Synch Prediv   = Value according to source clock
-      - OutPut         = Output Disable
-      - OutPutPolarity = High Polarity
-      - OutPutType     = Open Drain */ 
-  RtcHandle.Instance = RTC; 
-  RtcHandle.Init.HourFormat = RTC_HOURFORMAT_24;
-  RtcHandle.Init.AsynchPrediv = RTC_ASYNCH_PREDIV;
-  RtcHandle.Init.SynchPrediv = RTC_SYNCH_PREDIV;
-  RtcHandle.Init.OutPut = RTC_OUTPUT_DISABLE;
-  RtcHandle.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
-  RtcHandle.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
-  __HAL_RTC_RESET_HANDLE_STATE(&RtcHandle);
-  if (HAL_RTC_Init(&RtcHandle) != HAL_OK)
-  {
-    /* Initialization Error */
-    Error_Handler();
-  }
-  
-  /*##-2- Check if Data stored in BackUp register1: No Need to reconfigure RTC#*/
-  /* Read the Back Up Register 1 Data */
-  if (HAL_RTCEx_BKUPRead(&RtcHandle, RTC_BKP_DR1) != 0x32F2)
-  {
-    /* Configure RTC Calendar */
-    RTC_CalendarConfig();
-  }
-  else
-  {
-    /* Check if the Power On Reset flag is set */
-    if (__HAL_RCC_GET_FLAG(RCC_FLAG_PORRST) != RESET)
-    {
-      /* Turn on LED2: Power on reset occurred */
-      //BSP_LED_On(LED2);
-    }
-    /* Check if Pin Reset flag is set */
-    if (__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST) != RESET)
-    {
-      /* Turn on LED1: External reset occurred */
-      //BSP_LED_On(LED1);
-    }
-    /* Clear source Reset Flag */
-    __HAL_RCC_CLEAR_RESET_FLAGS();
-  }
-  
+  RTC_PeripheralConfig();
   RTC_CalendarConfig();
 
 #ifdef RTE_CMSIS_RTOS2
@@ -269,71 +209,6 @@ static void Error_Handler(void)
   while(1)
   {
   }
-}
-
-/**
-  * @brief  Configure the current time and date.
-  * @param  None
-  * @retval None
-  */
-void RTC_CalendarConfig(void)
-{
-  RTC_DateTypeDef sdatestructure;
-  RTC_TimeTypeDef stimestructure;
-
-  /*##-1- Configure the Date #################################################*/
-  /* Set Date: Tuesday February 18th 2014 */
-  sdatestructure.Year = 0x23;
-  sdatestructure.Month = RTC_MONTH_MARCH;
-  sdatestructure.Date = 0x8;
-  sdatestructure.WeekDay = RTC_WEEKDAY_WEDNESDAY;
-  
-  if(HAL_RTC_SetDate(&RtcHandle,&sdatestructure,RTC_FORMAT_BCD) != HAL_OK)
-  {
-    /* Initialization Error */
-    Error_Handler();
-  }
-
-  /*##-2- Configure the Time #################################################*/
-  /* Set Time: 02:00:00 */
-  stimestructure.Hours = 0x00;
-  stimestructure.Minutes = 0x00;
-  stimestructure.Seconds = 0x00;
-  stimestructure.TimeFormat = RTC_HOURFORMAT12_AM;
-  stimestructure.DayLightSaving = RTC_DAYLIGHTSAVING_NONE ;
-  stimestructure.StoreOperation = RTC_STOREOPERATION_RESET;
-
-  if (HAL_RTC_SetTime(&RtcHandle, &stimestructure, RTC_FORMAT_BCD) != HAL_OK)
-  {
-    /* Initialization Error */
-    Error_Handler();
-  }
-
-  /*##-3- Writes a data in a RTC Backup data Register1 #######################*/
-  HAL_RTCEx_BKUPWrite(&RtcHandle, RTC_BKP_DR1, 0x32F2);
-}
-
-/**
-  * @brief  Display the current time and date.
-  * @param  showtime : pointer to buffer
-  * @param  showdate : pointer to buffer
-  * @retval None
-  */
-void RTC_CalendarShow(uint8_t *showtime, uint8_t *showdate)
-{
-  RTC_DateTypeDef sdatestructureget;
-  RTC_TimeTypeDef stimestructureget;
-
-  /* Get the RTC current Time */
-  HAL_RTC_GetTime(&RtcHandle, &stimestructureget, RTC_FORMAT_BIN);
-  /* Get the RTC current Date */
-  HAL_RTC_GetDate(&RtcHandle, &sdatestructureget, RTC_FORMAT_BIN);
-  /* Display time Format : hh:mm:ss */
-  sprintf((char *)showtime, "%2d:%2d:%2d", stimestructureget.Hours, stimestructureget.Minutes, stimestructureget.Seconds);
-  /* Display date Format : mm-dd-yy */
-  sprintf((char *)showdate, "%2d-%2d-%2d", sdatestructureget.Month, sdatestructureget.Date, 2000 + sdatestructureget.Year);
-  EscribeFrase((char *)showtime,0);
-  EscribeFrase((char *)showdate,1);
 }
 
 #ifdef  USE_FULL_ASSERT
---
+++ Practica 2/rtc.h
@@ -0,0 +1,16 @@
+#ifndef _RTC_H
+#define _RTC_H
+
+#include "main.h"
+#include "lcd.h"
+#include <stdio.h>
+
+#define RTC_ASYNCH_PREDIV  0x7F   /* LSE as RTC clock */
+#define RTC_SYNCH_PREDIV   0x00FF /* LSE as RTC clock */
+
+/* RTC function prototypes ---------------------------------------------------*/
+void RTC_PeripheralConfig(void);
+void RTC_CalendarConfig(void);
+void RTC_CalendarShow(uint8_t *showtime, uint8_t *showdate);
+
+#endif
